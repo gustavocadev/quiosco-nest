6cbaa83c6e48ac2d3769795bf2a25fb245ed7a79
gustavocadev <churaalaygustavo11@gmail.com>
2022-10-25 10:05:03 -0500

    I've added some endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuioscoModule } from './quiosco/quiosco.module';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [QuioscoModule],
+  imports: [QuioscoModule, UsersModule, OrdersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
